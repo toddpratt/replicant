15bbfcbd6790743993ed43258c1d71bea889e641
Todd Pratt <ae88925@gmail.com>
2014-01-08 08:03:49 -0500

    op granting capabilities

--- cmdirc.py
+++ cmdirc.py
@@ -13,5 +13,33 @@ class IrcCommand(cmdbase.BaseAdminCommand):
     else:
       request.respond('%s: no method' % method_name)
 
+class OpCommand(cmdbase.BaseAdminCommand):
+
+  def handle_admin(self, request):
+    request.proto.mode(request.channel, True, 'o', user=request.args[1])
+
+class OpmeCommand(cmdbase.BaseAdminCommand):
+
+  def handle_user(self, request):
+    query = 'SELECT COUNT(*) FROM ops WHERE channel = ? AND account = ?'
+
+    def success(results, request):
+      if results[0][0] == 1:
+        request.proto.mode(request.channel, True, 'o', user=request.nick)
+      else:
+        request.respond('you aint no op I ever heard of')
+
+    def failure(failure, request):
+      request.respond('error: %s' % failure.getErrorMessage())
+
+    d = request.db.runQuery(query, (request.channel, request.account))
+    d.addCallback(success, request)
+    d.addErrback(failure, request)
+
+  def handle_admin(self, request):
+    self.handle_user(request)
+
 def register_commands():
   command.CommandHandler.register('irc', IrcCommand())
+  command.CommandHandler.register('op', OpCommand())
+  command.CommandHandler.register('opme', OpmeCommand())
