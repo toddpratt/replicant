227264844fef93b82ca602bc9df61b3a05efc1ab
Todd Pratt <ae88925@gmail.com>
2014-04-30 20:15:59 -0400

    cleanup autoop module

--- plugin_autoop.py
+++ plugin_autoop.py
@@ -5,10 +5,7 @@ class JoinPlugin(object):
 
   def irc_JOIN(self, proto, fulluser, channels):
     req = request.Request(fulluser, channels[0], '', None, proto)
-    print 'joined:', req.user
-    print 'auto-op:', proto.factory.conf['plugins']['auto-op']
     if req.user in proto.factory.conf['plugins']['auto-op']:
-      print 'opping'
       proto.mode(channels[0], True, 'o', user=req.nick)
 
 def register():
