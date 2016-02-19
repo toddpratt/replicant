407abe0196cf216d36af4b5004126022751f795c
Todd A. Pratt <toddpratt978@gmail.com>
2016-02-18 21:29:58 -0500

    cleanup the api module

--- plugin_api.py
+++ plugin_api.py
@@ -8,9 +8,10 @@ from twisted.web import client
 class JsonApiCommand(plugin_base.BaseCommand):
 
   def compute_url(self, request):
-    config = request.conf['plugins']['weather']
-    return (config["url"] + "?zip=" + request.args[1] +
-            "&appid=" + config["key"] + "&units=imperial")
+    return "url"
+
+  def report_success(self, results, request):
+    request.respond("it worked.")
 
   def handle_user(self, request):
     d = client.getPage(self.compute_url(request).encode('utf-8'))
@@ -20,20 +21,5 @@ class JsonApiCommand(plugin_base.BaseCommand):
   def _report_success(self, results, request):
     self.report_success(json.loads(results), request)
 
-  def report_success(self, results, request):
-    city = results["name"]
-    description = results["weather"][0]["description"]
-    temp = results["main"]["temp"]
-    humidity = results["main"]["humidity"]
-    request.respond("City: %(city)s: Temp: %(temp)sF "
-                    "Humidity: %(humidity)s%% "
-                    "conditions: %(description)s" % {
-                        "city": city, "description": description,
-                        "temp": temp, "humidity": humidity})
-
   def report_failure(self, failure, request):
     request.respond('error: %s' % failure.getErrorMessage())
-
-
-def register():
-  command.CommandHandler.register('w', WeatherCommand())
