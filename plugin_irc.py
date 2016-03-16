c257a46b9f64e46d19a358dbdfc0bc76fb3bbabd
Todd A. Pratt <toddpratt978@gmail.com>
2016-03-16 07:42:36 -0400

    migrate irc module away from database

--- plugin_irc.py
+++ plugin_irc.py
@@ -1,29 +1,7 @@
+import json
+
 import plugin_base
 import command
-
-import hashlib
-import random
-import sys
-
-challenges = {}
-password = "hard4u2Ges"
-
-class AuthCommand(plugin_base.BaseCommand):
-
-  def handle_user(self, request):
-    value = challenges.get(request.account)
-    if value and len(request.args) == 2 and value == request.args[1]:
-      request.users.append(request.account)
-      request.respond("ok")
-    elif value:
-      del challenges[request.account]
-      request.respond("challenge aborted.")
-    else:
-      md5 = hashlib.md5(
-              str(random.randint(sys.maxint/2, sys.maxint))).hexdigest()
-      challenges[request.account] = hashlib.md5(
-              password + md5 + '\n').hexdigest()
-      request.respond(md5)
 
 class IrcCommand(plugin_base.BaseAdminCommand):
 
@@ -32,6 +10,7 @@ class IrcCommand(plugin_base.BaseAdminCommand):
     args = request.args[2:]
     method = getattr(request.proto, method_name, None)
     if method:
+      args = [json.loads(s) for s in args]
       method(*args)
       request.respond('Okay')
     else:
@@ -39,43 +18,20 @@ class IrcCommand(plugin_base.BaseAdminCommand):
 
 class OpCommand(plugin_base.BaseAdminCommand):
 
-  def handle_user(self, request):
+  def handle_admin(self, request):
     request.proto.mode(request.channel, True, 'o', user=request.args[1])
 
 class OpmeCommand(plugin_base.BaseAdminCommand):
 
   def handle_user(self, request):
-    query = 'SELECT COUNT(*) FROM ops WHERE channel = ? AND account = ?'
-
-    def success(results, request):
-      if results[0][0] == 1:
-        request.proto.mode(request.channel, True, 'o', user=request.nick)
-      else:
-        request.respond('you ain\'t no op I ever heard of')
-
-    def failure(failure, request):
-      request.respond('error: %s' % failure.getErrorMessage())
-
-    d = self.get_db().runQuery(query, (request.channel, request.account))
-    d.addCallback(success, request)
-    d.addErrback(failure, request)
+    ch_cfg = self._catalog.get_channel_config(request.chatnet, request.channel)
+    if request.account in ch_cfg["operators"]:
+      request.proto.mode(request.channel, True, 'o', user=request.nick)
+    else:
+      request.respond('you ain\'t no op I ever heard of')
 
   def handle_admin(self, request):
-    self.handle_user(request)
-
-class OpsCommand(plugin_base.BaseCommand):
-
-  def handle_user(self, request):
-    query = 'SELECT account FROM ops WHERE channel = ?'
-
-    def success(results, request):
-      if results:
-        request.respond('ops: ' + ', '.join(e[0] for e in results))
-      else:
-        request.respond('I don\'t know of any ops on this channel.')
-
-    d = self.get_db().runQuery(query, (request.channel, ))
-    d.addCallback(success, request)
+    request.proto.mode(request.channel, True, 'o', user=request.nick)
 
 class InviteCommand(plugin_base.BaseCommand):
 
@@ -86,5 +42,4 @@ def register(catalog):
   command.CommandHandler.register('auth', AuthCommand(catalog))
   command.CommandHandler.register('irc', IrcCommand(catalog))
   command.CommandHandler.register('op', OpCommand(catalog))
-  command.CommandHandler.register('ops', OpsCommand(catalog))
   command.CommandHandler.register('opme', OpmeCommand(catalog))
