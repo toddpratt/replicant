a69f7fe4f1ef89ab6ce2fb9893a9c6528b966e7a
Todd A. Pratt <toddpratt978@gmail.com>
2016-03-23 20:10:34 -0400

    tell command

---
+++ plugin_tell.py
@@ -0,0 +1,39 @@
+import collections
+import command
+import plugin_base
+import proto
+import request
+
+
+class JoinPlugin(object):
+
+  def __init__(self, catalog):
+    self._catalog = catalog
+
+  def irc_JOIN(self, proto, fulluser, channels):
+    req = request.Request(fulluser, channels[0], '', None, proto,
+            proto.factory.ircnet)
+    messages = self._catalog.get("tells").get(req.nick, [])
+    for message in messages:
+      req.respond(message)
+    del self._catalog.get("tells")[req.nick]
+
+class TellCommand(plugin_base.BaseCommand):
+
+  def handle_user(self, req):
+    _, nicks, msg = req.message.split(None, 2)
+    nicks = nicks.split(',')
+    for nick in nicks:
+      self._catalog.get("tells").setdefault(nick, [])
+      self._catalog.get("tells")[nick].append(msg)
+    req.respond("will do")
+
+class ShowUsersCommand(plugin_base.BaseCommand):
+
+  def handle_user(self, req):
+    req.respond(", ".join(self._catalog.get("tells").iterkeys()))
+
+def register(catalog):
+  command.CommandHandler.register('tell', TellCommand(catalog))
+  command.CommandHandler.register('tells', ShowUsersCommand(catalog))
+  proto.BotProtocol.register_plugin(JoinPlugin(catalog))
